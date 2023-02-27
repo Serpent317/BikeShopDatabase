d56c03669c0428542a50d1126c037643b896e7b0
Serpent317 <70720285+Serpent317@users.noreply.github.com>
2023-02-27 14:41:13 -0500

    Add files via upload

---
+++ BikeShop/BikeShop/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace BikeShop
+{
+    partial class OrderForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StoreLabel = new System.Windows.Forms.Label();
+            this.StaffLabel = new System.Windows.Forms.Label();
+            this.CustomerFNameLabel = new System.Windows.Forms.Label();
+            this.DiscountLabel = new System.Windows.Forms.Label();
+            this.CustomerLNameLabel = new System.Windows.Forms.Label();
+            this.BikeNameLabel = new System.Windows.Forms.Label();
+            this.BikeQuantityLabel = new System.Windows.Forms.Label();
+            this.CompleteOrderButton = new System.Windows.Forms.Button();
+            this.ItemsOnOrderLabel = new System.Windows.Forms.Label();
+            this.CustomerFNameTextBox = new System.Windows.Forms.TextBox();
+            this.CustomerLNameTextBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // StoreLabel
+            // 
+            this.StoreLabel.AutoSize = true;
+            this.StoreLabel.Location = new System.Drawing.Point(33, 25);
+            this.StoreLabel.Name = "StoreLabel";
+            this.StoreLabel.Size = new System.Drawing.Size(53, 25);
+            this.StoreLabel.TabIndex = 0;
+            this.StoreLabel.Text = "Store";
+            this.StoreLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // StaffLabel
+            // 
+            this.StaffLabel.AutoSize = true;
+            this.StaffLabel.Location = new System.Drawing.Point(175, 25);
+            this.StaffLabel.Name = "StaffLabel";
+            this.StaffLabel.Size = new System.Drawing.Size(48, 25);
+            this.StaffLabel.TabIndex = 1;
+            this.StaffLabel.Text = "Staff";
+            // 
+            // CustomerFNameLabel
+            // 
+            this.CustomerFNameLabel.AutoSize = true;
+            this.CustomerFNameLabel.Location = new System.Drawing.Point(477, 23);
+            this.CustomerFNameLabel.Name = "CustomerFNameLabel";
+            this.CustomerFNameLabel.Size = new System.Drawing.Size(179, 25);
+            this.CustomerFNameLabel.TabIndex = 2;
+            this.CustomerFNameLabel.Text = "Customer First Name";
+            // 
+            // DiscountLabel
+            // 
+            this.DiscountLabel.AutoSize = true;
+            this.DiscountLabel.Location = new System.Drawing.Point(317, 25);
+            this.DiscountLabel.Name = "DiscountLabel";
+            this.DiscountLabel.Size = new System.Drawing.Size(82, 25);
+            this.DiscountLabel.TabIndex = 3;
+            this.DiscountLabel.Text = "Discount";
+            // 
+            // CustomerLNameLabel
+            // 
+            this.CustomerLNameLabel.AutoSize = true;
+            this.CustomerLNameLabel.Location = new System.Drawing.Point(691, 23);
+            this.CustomerLNameLabel.Name = "CustomerLNameLabel";
+            this.CustomerLNameLabel.Size = new System.Drawing.Size(177, 25);
+            this.CustomerLNameLabel.TabIndex = 4;
+            this.CustomerLNameLabel.Text = "Customer Last Name";
+            // 
+            // BikeNameLabel
+            // 
+            this.BikeNameLabel.AutoSize = true;
+            this.BikeNameLabel.Location = new System.Drawing.Point(33, 156);
+            this.BikeNameLabel.Name = "BikeNameLabel";
+            this.BikeNameLabel.Size = new System.Drawing.Size(96, 25);
+            this.BikeNameLabel.TabIndex = 5;
+            this.BikeNameLabel.Text = "Bike Name";
+            // 
+            // BikeQuantityLabel
+            // 
+            this.BikeQuantityLabel.AutoSize = true;
+            this.BikeQuantityLabel.Location = new System.Drawing.Point(239, 156);
+            this.BikeQuantityLabel.Name = "BikeQuantityLabel";
+            this.BikeQuantityLabel.Size = new System.Drawing.Size(80, 25);
+            this.BikeQuantityLabel.TabIndex = 6;
+            this.BikeQuantityLabel.Text = "Quantity";
+            // 
+            // CompleteOrderButton
+            // 
+            this.CompleteOrderButton.Location = new System.Drawing.Point(33, 334);
+            this.CompleteOrderButton.Name = "CompleteOrderButton";
+            this.CompleteOrderButton.Size = new System.Drawing.Size(166, 41);
+            this.CompleteOrderButton.TabIndex = 7;
+            this.CompleteOrderButton.Text = "Complete Order";
+            this.CompleteOrderButton.UseVisualStyleBackColor = true;
+            // 
+            // ItemsOnOrderLabel
+            // 
+            this.ItemsOnOrderLabel.AutoSize = true;
+            this.ItemsOnOrderLabel.Location = new System.Drawing.Point(33, 120);
+            this.ItemsOnOrderLabel.Name = "ItemsOnOrderLabel";
+            this.ItemsOnOrderLabel.Size = new System.Drawing.Size(137, 25);
+            this.ItemsOnOrderLabel.TabIndex = 8;
+            this.ItemsOnOrderLabel.Text = "Items on Order:";
+            // 
+            // CustomerFNameTextBox
+            // 
+            this.CustomerFNameTextBox.Location = new System.Drawing.Point(477, 70);
+            this.CustomerFNameTextBox.Name = "CustomerFNameTextBox";
+            this.CustomerFNameTextBox.Size = new System.Drawing.Size(163, 31);
+            this.CustomerFNameTextBox.TabIndex = 9;
+            // 
+            // CustomerLNameTextBox
+            // 
+            this.CustomerLNameTextBox.Location = new System.Drawing.Point(691, 70);
+            this.CustomerLNameTextBox.Name = "CustomerLNameTextBox";
+            this.CustomerLNameTextBox.Size = new System.Drawing.Size(205, 31);
+            this.CustomerLNameTextBox.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(33, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(114, 33);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(175, 70);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(114, 33);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(33, 203);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(114, 33);
+            this.comboBox3.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(239, 203);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 31);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(317, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(80, 31);
+            this.textBox2.TabIndex = 16;
+            // 
+            // OrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 422);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.CustomerLNameTextBox);
+            this.Controls.Add(this.CustomerFNameTextBox);
+            this.Controls.Add(this.ItemsOnOrderLabel);
+            this.Controls.Add(this.CompleteOrderButton);
+            this.Controls.Add(this.BikeQuantityLabel);
+            this.Controls.Add(this.BikeNameLabel);
+            this.Controls.Add(this.CustomerLNameLabel);
+            this.Controls.Add(this.DiscountLabel);
+            this.Controls.Add(this.CustomerFNameLabel);
+            this.Controls.Add(this.StaffLabel);
+            this.Controls.Add(this.StoreLabel);
+            this.Name = "OrderForm";
+            this.Text = "Order";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label StoreLabel;
+        private Label StaffLabel;
+        private Label CustomerFNameLabel;
+        private Label DiscountLabel;
+        private Label CustomerLNameLabel;
+        private Label BikeNameLabel;
+        private Label BikeQuantityLabel;
+        private Button CompleteOrderButton;
+        private Label ItemsOnOrderLabel;
+        private TextBox CustomerFNameTextBox;
+        private TextBox CustomerLNameTextBox;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
---
+++ BikeShop/BikeShop/Form1.cs
@@ -0,0 +1,25 @@
+namespace BikeShop
+{
+    public partial class OrderForm : Form
+    {
+        public OrderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
